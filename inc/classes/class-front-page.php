ca8f12dddcc3fbbc3a6fba1733777d09a03376e6
sasha23rus <sasha233rus@gmail.com>
2021-12-14 06:19:07 +0300

    end front page

--- inc/classes/class-front-page.php
+++ inc/classes/class-front-page.php
@@ -62,11 +62,11 @@ class Front_Page extends Template {
 				$post_id = $post->ID;
 
 				$item = array (
-					'title' => get_the_title( $post_id ),
-					'icon'  => get_field( 'possibilities_icon', $post_id ) ?? false,
-					'text'  => get_field( 'possibilities_text', $post_id ) ?? false,
+					'title'   => get_the_title( $post_id ),
+					'icon'    => get_field( 'possibilities_icon', $post_id ) ?? false,
+					'text'    => get_field( 'possibilities_text', $post_id ) ?? false,
 					'row_end' => ($i==2)?'col_last':false,
-					'clear' => ($i==2)?true:false
+					'clear'   => ($i==2)?true:false
 				);
 
 				array_push( $args, $item );
@@ -111,35 +111,97 @@ class Front_Page extends Template {
 		return $this->render( $template, $args );
 	}
 	
-	public function get_section_blog_title(){
+	public function get_section_blog(){
 		$template = '/template-parts/front-page/blog.php';
 		
 		$post_type_args = array(
 			'post_type'      => 'post',
-			'category'    => 0,
+			'category'       => 0,
 			'post_status'    => 'publish',
 			'posts_per_page' => 4,
-			'order'=> 'ASC',
+			'order'          => 'ASC',
 		);
 		
 		$posts = get_posts( $post_type_args );
 		$args = array(
-			'blog_title' => get_field( 'blog_section_title' ) ?? false,
+			'blog_title' => get_theme_mod( 'blog_title'  ) ?? false,
 		);
 		
 		if ( $posts ) {
 			foreach ( $posts as $post ) {
 				$post_id = $post->ID;
-				$post_link = get_post_permalink($post_id);
-				
+				$comments_count = wp_count_comments($post_id);
 				$item[] = array (
-					'post_link' => $post_link,
-					'title' => get_the_title( $post_id ),
-					//'image'  => get_field( 'image', $post_id ) ?? false,
-					//'link'  => get_field( 'link', $post_id ) ?? false,
+					'post_link'      => get_post_permalink($post_id),
+					'title'          => get_the_title( $post_id ),
+					'image'          => get_the_post_thumbnail_url( $post_id, 'thumbnail' ) ?? false,
+					'date'           => get_the_date( 'j F Y', $post_id ),
+					'excerpt'        => get_the_excerpt($post_id) ?? false,
+					'count_comments' => $comments_count->total_comments ?? false,
 				);
 			}
 			$args['posts'] = $item;
+		}
+		
+		return $this->render( $template, $args );
+	}
+	
+	public function get_section_team(){
+		$template = '/template-parts/front-page/team.php';
+		
+		$post_type_args = array(
+			'post_type'      => 'team',
+			'post_status'    => 'publish',
+			'posts_per_page' => 4,
+			'order'          => 'ASC',
+		);
+		
+		$posts = get_posts( $post_type_args );
+		$args = array(
+			'team_title' => get_theme_mod( 'team_title' ) ?? false,
+		);
+		
+		if ( $posts ) {
+			foreach ( $posts as $post ) {
+				$post_id = $post->ID;
+				
+				$item[] = array (
+					'name'      => get_the_title( $post_id ),
+					'image'     => get_field( 'image', $post_id ) ?? false,
+					'posititon' => get_field( 'posititon', $post_id ) ?? false,
+				);
+			}
+			$args['posts'] = $item;
+		}
+		
+		return $this->render( $template, $args );
+	}
+	
+	public function get_section_clients(){
+		$template = '/template-parts/front-page/clients.php';
+		
+		$post_type_args = array(
+			'post_type'      => 'clients',
+			'post_status'    => 'publish',
+			'posts_per_page' => 20,
+			'order'          => 'ASC',
+		);
+		
+		$args = array();
+		
+		$posts = get_posts( $post_type_args );
+		
+		if ( $posts ) {
+			foreach ( $posts as $post ) {
+				$post_id = $post->ID;
+				
+				$item = array (
+					'name'      => get_the_title( $post_id ),
+					'url' => get_field( 'url', $post_id ) ?? false,
+					'image'     => get_field( 'image', $post_id ) ?? false,
+				);
+				array_push( $args, $item );
+			}
 		}
 		
 		return $this->render( $template, $args );
--- template-parts/front-page/blog.php
+++ template-parts/front-page/blog.php
@@ -5,7 +5,6 @@
  * @var $args;
  */
 ?>
-хз почему он заголовок не выводит
 <?php if ($args): ?>
     <?php if($args['blog_title']): ?>
         <div class="section notopmargin notopborder">
@@ -21,79 +20,38 @@
         <div class="container clear-bottommargin clearfix">
         <div class="row">
     
-            <?php foreach ($args['posts'] as $item) ?>
+            <?php foreach ($args['posts'] as $item): ?>
             <div class="col-lg-3 col-md-6 bottommargin">
                 <div class="ipost clearfix">
-                    <div class="entry-image">
-                        <a href="#"><img class="image_fade" src="images/magazine/thumb/1.jpg" alt="Image"></a>
-                    </div>
+                    <?php if ($item['image']): ?>
+                        <div class="entry-image">
+                            <a href="<?php echo esc_url( $item['post_link'] );?>">
+                                <img class="image_fade" src="<?php echo esc_attr( $item['image'] );?>" alt="<?php echo esc_attr($item['title'])?>">
+                            </a>
+                        </div>
+                    <?php endif; ?>
+                    
                     <div class="entry-title">
-                        <h3><a href="blog-single.html">Bloomberg smart cities; change-makers economic security</a></h3>
+                        <h3><a href="<?php esc_url( $item['post_link'] );?>"><?php echo esc_html($item['title'])?></a></h3>
                     </div>
+                    
                     <ul class="entry-meta clearfix">
-                        <li><i class="icon-calendar3"></i> 13th Jun 2014</li>
-                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 53</a></li>
+                        <li><i class="icon-calendar3"></i> <?php echo esc_html($item['date'])?></li>
+                        <?php if ($item['count_comments']): ?>
+                            <li>
+                                <a href="<?php echo esc_url( $item['post_link'] );?>#comments"><i class="icon-comments"></i><?php echo esc_html($item['count_comments'])?></a>
+                            </li>
+                        <?php endif; ?>
                     </ul>
-                    <div class="entry-content">
-                        <p>Prevention effect, advocate dialogue rural development lifting people up community civil society. Catalyst, grantees leverage.</p>
-                    </div>
+                    
+                    <?php if ($item['excerpt']): ?>
+                        <div class="entry-content">
+                            <p><?php echo esc_html($item['excerpt']);?></p>
+                        </div>
+                    <?php endif; ?>
                 </div>
             </div>
-            <?/*
-            <div class="col-lg-3 col-md-6 bottommargin">
-                <div class="ipost clearfix">
-                    <div class="entry-image">
-                        <a href="#"><img class="image_fade" src="images/magazine/thumb/2.jpg" alt="Image"></a>
-                    </div>
-                    <div class="entry-title">
-                        <h3><a href="blog-single.html">Medicine new approaches communities, outcomes partnership</a></h3>
-                    </div>
-                    <ul class="entry-meta clearfix">
-                        <li><i class="icon-calendar3"></i> 24th Feb 2014</li>
-                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 17</a></li>
-                    </ul>
-                    <div class="entry-content">
-                        <p>Cross-agency coordination clean water rural, promising development turmoil inclusive education transformative community.</p>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="col-lg-3 col-md-6 bottommargin">
-                <div class="ipost clearfix">
-                    <div class="entry-image">
-                        <a href="#"><img class="image_fade" src="images/magazine/thumb/3.jpg" alt="Image"></a>
-                    </div>
-                    <div class="entry-title">
-                        <h3><a href="blog-single.html">Significant altruism planned giving insurmountable challenges liberal</a></h3>
-                    </div>
-                    <ul class="entry-meta clearfix">
-                        <li><i class="icon-calendar3"></i> 30th Dec 2014</li>
-                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 13</a></li>
-                    </ul>
-                    <div class="entry-content">
-                        <p>Micro-finance; vaccines peaceful contribution citizens of change generosity. Measures design thinking accelerate progress medical initiative.</p>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="col-lg-3 col-md-6 bottommargin">
-                <div class="ipost clearfix">
-                    <div class="entry-image">
-                        <a href="#"><img class="image_fade" src="images/magazine/thumb/4.jpg" alt="Image"></a>
-                    </div>
-                    <div class="entry-title">
-                        <h3><a href="blog-single.html">Compassion conflict resolution, progressive; tackle</a></h3>
-                    </div>
-                    <ul class="entry-meta clearfix">
-                        <li><i class="icon-calendar3"></i> 15th Jan 2014</li>
-                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 54</a></li>
-                    </ul>
-                    <div class="entry-content">
-                        <p>Community health workers best practices, effectiveness meaningful work The Elders fairness. Our ambitions local solutions globalization.</p>
-                    </div>
-                </div>
-            </div>
-            <?*/?>
+            <?php endforeach; ?>
         </div>
     </div>
     <?php endif;?>
