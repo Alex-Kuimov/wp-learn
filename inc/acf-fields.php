93e8639b584dc4b23ca5707b035d1f512b80180d
sasha23rus <sasha233rus@gmail.com>
2021-12-14 06:27:42 +0300

    ACF fields add

--- inc/acf-fields.php
+++ inc/acf-fields.php
@@ -613,5 +613,70 @@ icon-line2-equalizer',
 			'description' => '',
 			'show_in_rest' => 0,
 		));
-	
+		//clietns sectin
+		acf_add_local_field_group(array(
+			'key' => 'group_61b8091a7abc4',
+			'title' => 'Clients',
+			'fields' => array(
+				array(
+					'key' => 'field_61b809230272c',
+					'label' => 'URL',
+					'name' => 'url',
+					'type' => 'url',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => 'https://ya.ru',
+					'placeholder' => '',
+				),
+				array(
+					'key' => 'field_61b809420272d',
+					'label' => 'Image',
+					'name' => 'image',
+					'type' => 'image',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'return_format' => 'url',
+					'preview_size' => 'medium',
+					'library' => 'all',
+					'min_width' => '',
+					'min_height' => '',
+					'min_size' => '',
+					'max_width' => '',
+					'max_height' => '',
+					'max_size' => '',
+					'mime_types' => '',
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'clients',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'normal',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => true,
+			'description' => '',
+			'show_in_rest' => 0,
+		));
 	endif;
+	
