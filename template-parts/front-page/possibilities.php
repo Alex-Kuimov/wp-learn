36c89981a648f66ef0c74c1c06b015b0a475d247
Alex Kuimov <spoot@bk.ru>
2021-12-14 22:02:16 +0300

    Fix home work lesson05

--- template-parts/front-page/possibilities.php
+++ template-parts/front-page/possibilities.php
@@ -9,30 +9,32 @@
 <?php if ( $args ) : ?>
 
 <div class="container clearfix">
-	<?php foreach ( $args as $item ) : ?>
-		<div class="col_one_third <?php echo esc_attr( $item['row_end'] ); ?>">
+	<?php foreach ( $args as $key => $post ) : ?>
+
+		<div class="col_one_third <?php echo esc_attr( $this->get_start_class( $key ) ); ?>">
+
 			<div class="feature-box fbox-small fbox-plain" data-animate="fadeIn">
 
-				<?php if ( $item['icon'] ) : ?>
+				<?php if ( get_field( 'possibilities_icon', $post->ID ) ) : ?>
 					<div class="fbox-icon">
-						<a href="#"><i class="<?php echo esc_attr( $item['icon'] ); ?>"></i></a>
+						<a href="#"><i class="<?php echo esc_attr( get_field( 'possibilities_icon', $post->ID ) ); ?>"></i></a>
 					</div>
 				<?php endif; ?>
 
-				<?php if ( $item['title'] ) : ?>
-					<h3><?php echo esc_html( $item['title'] ); ?></h3>
-				<?php endif; ?>
+				<h3><?php echo esc_html( get_the_title( $post->ID ) ); ?></h3>
 
-				<?php if ( $item['text'] ) : ?>
-					<p><?php echo esc_html( $item['text'] ); ?></p>
+
+				<?php if ( get_field( 'possibilities_text', $post->ID ) ) : ?>
+					<p><?php echo esc_html( get_field( 'possibilities_text', $post->ID ) ); ?></p>
 				<?php endif; ?>
 
 			</div>
 		</div>
-		
-		<?php if ( $item['clear'] ) : ?>
+
+		<?php if ( $this->get_end_row( $key ) ) : ?>
 			<div class="clear"></div>
 		<?php endif; ?>
+
 	<?php endforeach; ?>
 </div>
 
